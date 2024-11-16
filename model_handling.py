8eeaebe2215d6db084597c329793ad185315b8a2
alex-rowden <dr.alex.rowden@gmail.com>
2024-11-16 18:21:25 -0500

    When Alex learned that VLLM doesn't work on Windows :(

--- model_handling.py
+++ model_handling.py
@@ -1,7 +1,10 @@
+'''Module to handle models from various sources to be hosted through the API'''
 #first part imports
-from typing import Dict, List, AsyncGenerator, Generator
+from typing import Dict, List, AsyncGenerator
 from abc import ABC, abstractmethod
 import uuid
+import json
+import os
 #third-party imports
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, BatchEncoding, TextStreamer
@@ -15,8 +18,9 @@ class LLM(ABC):
         '''Abstract Initialization function for Generic LLM Class to setup a CausalLM model and
          tokenizer'''
     @abstractmethod
-    def generate_text(self, message_chain: List[Dict[str, str]], max_length : int =50,
-                      num_return_sequences: int =1, max_new_tokens : int = 4096) -> str:
+    def generate_text(self, message_chain: List[Dict[str, str]],
+                    max_new_tokens : int = 4096, **kwargs) -> str:
+
         '''Abstract Method to generate text from a prompt using the LLM'''
 
 class HFLLM(LLM):
@@ -31,9 +35,15 @@ class HFLLM(LLM):
             device_map = device_map,
             torch_dtype = torch_dtype
         )
-    def generate_text(self, message_chain: List[Dict[str, str]], max_length : int =50,
-                      num_return_sequences: int =1, max_new_tokens : int = 4096) -> str:
+    def generate_text(self, message_chain: List[Dict[str, str]],
+                    max_new_tokens : int = 4098,
+                    **kwargs
+                    ) -> str:
         '''Function to generate text from a prompt using the LLM'''
+        #process kwargs
+        num_return_sequences: int = \
+            kwargs['num_return_sequences'] if 'num_return_sequences' in kwargs else 1
+        max_length: int = kwargs['max_length'] if 'max_length' in kwargs else 4096
         prompt:  List[int] | Dict[str, str] = self.tokenizer.apply_chat_template(
             message_chain,
             tokenize=False,
@@ -43,8 +53,9 @@ class HFLLM(LLM):
             prompt,
             return_tensors="pt",
             padding=True,
-            truncation=True, 
+            truncation=True,
             max_length=max_length
+
         )
         input_ids: torch.Tensor = inputs.to(self.model.device)
         streamer: TextStreamer = TextStreamer(
@@ -61,73 +72,67 @@ class HFLLM(LLM):
         )
         output_text: str = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
         return output_text
+if os.name != 'nt':
+    class VLLM(LLM):
+        '''LLM Subclass which uses vLLM's inference engine for faster inference
+        and lower memory usage'''
 
-class VLLM(LLM):
-    '''LLM Subclass which uses vLLM's inference engine for faster inference
-      and lower memory usage'''
-
-    def __init__(self, model_name: str, device_map: str = "auto",
-                  dtype: str = 'half', streaming = True) -> None:
-        '''Initialization function for vLLM LLM Class to setup a CausalLM model and tokenizer'''
-        engine_args: vllm.EngineArgs = vllm.EngineArgs(
-            model=model_name,
-            dtype=dtype,
-            enable_prefix_caching=True
-        )
-        if streaming:
-            self.model = vllm.ASyncLLMEngine.from_engine_args(engine_args)
-        else:
-            self.model = vllm.LLMEngine.from_engine_args(engine_args)
-        self.streaming: bool = streaming
-    def message_chain_to_string(self, message_chain: List[Dict[str, str]]) -> str:
-        '''Function to convert a message chain to a string'''
-        prompt: str = ""
-        for message in message_chain:
-            prompt += f"{message['role']}: {message['content']}\n"
-        return prompt
-    def generate_text(
-        self,
-        message_chain: List[Dict[str, str]],
-        max_new_tokens : int = 4096
-    ) -> str:
-        '''Function to generate text from a prompt using the LLM'''
-        prompt: str = self.message_chain_to_string(message_chain)
-        request_id: str = str(uuid.uuid().hex)
-        sampling_params : vllm.SamplingParams = vllm.SamplingParams(
-            temperature=0.2,
-            top_p=0.95,
-            max_tokens=max_new_tokens
-        )
-
-        model_response: vllm.Output = self.model.generate(prompt, sampling_params, request_id)
-        if self.streaming:
-            async def stream_response(model_response: vllm.Output) -> str:
-                '''Function to stream the response from the LLM'''
-                response: str = ""
-                async for request_out in model_response:
-                    
-                    response += token.text
-                    yield token.text
-                return response
-
-            return stream_response(model_response)
-        else:
-            return model_response.outputs[0].text
-
-
-        
-
-
-
-class SolarLLM(LLM):
+        def __init__(self, model_name: str, device_map: str = "auto",
+                    dtype: str = 'half', streaming = True) -> None:
+            '''Initialization function for vLLM LLM Class to setup a CausalLM model and tokenizer'''
+            engine_args: vllm.EngineArgs = vllm.EngineArgs(
+                model=model_name,
+                dtype=dtype,
+                enable_prefix_caching=True
+            )
+            if streaming:
+                self.model: vllm.AsyncLLMEngine = vllm.AsyncLLMEngine.from_engine_args(engine_args)
+            else:
+                self.model: vllm.LLMEngine = vllm.LLMEngine.from_engine_args(engine_args)
+            self.streaming: bool = streaming
+        def message_chain_to_string(self, message_chain: List[Dict[str, str]]) -> str:
+            '''Function to convert a message chain to a string'''
+            prompt: str = ""
+            for message in message_chain:
+                prompt += f"{message['role']}: {message['content']}\n"
+            return prompt
+        def generate_text(
+            self,
+            message_chain: List[Dict[str, str]],
+            max_new_tokens : int = 4096,
+            **kwargs
+        ) -> str:
+            '''Function to generate text from a prompt using the LLM'''
+            prompt: str = self.message_chain_to_string(message_chain)
+            request_id: str = str(uuid.uuid4().hex)
+            sampling_params : vllm.SamplingParams = vllm.SamplingParams(
+                temperature=0.2,
+                top_p=0.95,
+                max_tokens=max_new_tokens
+            )
+            model_response: vllm.RequestOutput = self.model.generate(prompt,
+                sampling_params, request_id)
+            if self.streaming:
+                async def stream_response(
+                        model_response: vllm.RequestOutput
+                        ) -> AsyncGenerator[bytes, None]:
+                    '''Function to stream the response from the LLM'''
+                    async for request_out in model_response:
+                        prompt: str = request_out.prompt
+                        text_outputs = [
+                            prompt + token.text for token in request_out.outputs
+                        ]
+                        ret: Dict[str, List[str]] = {"text": text_outputs}
+                        yield (json.dumps(ret, ensure_ascii=True).encode('utf-8'))
+                return stream_response(model_response)
+            else:
+                return model_response
+class SolarLLM(HFLLM):
     '''LLM Subclass for the Upstage Solar-10.7b-Instruct-v1.0 model'''
     def __init__(self, **kwargs) -> None:
         '''Constructor for SolarLLM. Just calls the superclass constructor
         with the correct model name'''
         super().__init__("Upstage/SOLAR-10.7b-Instruct-v1.0", **kwargs)
-
-
-
 if __name__ == "__main__":
     llm: LLM = SolarLLM(device_map = 'cuda:0')
     messages: List[Dict[str, str]] = [
